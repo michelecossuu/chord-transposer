7696d840ef30291adbf0077e72a079bbe7d72dcc
michelecossuu <michelecossu01@gmail.com>
2025-08-05 17:31:50 +0200

    Initial project setup with Maven and Spring Boot
    - Added basic project structure
    - Configured Maven wrapper for easy builds
    - Created main application and test classes
    - Set up application properties and dependencies
    - Included a README for project overview

---
+++ transposer/src/main/java/com/michelecossu/chords/transposer/TransposerApplication.java
@@ -0,0 +1,13 @@
+package com.michelecossu.chords.transposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransposerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransposerApplication.class, args);
+    }
+
+}
---
+++ transposer/src/test/java/com/michelecossu/chords/transposer/TransposerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.michelecossu.chords.transposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransposerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // TODO document why this method is empty
+    }
+
+}
